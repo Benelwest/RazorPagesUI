418c22e3ef7b3f3b2948345ee3c4d22fd863856c
ben <benell@bluewin.ch>
2020-06-04 22:39:14 +0200

    go2

--- RazorPagesUI/Pages/Forms/AddAdress.cshtml.cs
+++ RazorPagesUI/Pages/Forms/AddAdress.cshtml.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesUI.Models;
 
 namespace RazorPagesUI.Pages.Forms
 {
-    public class AddAdressModel : PageModel
+    public class AddAdressModel: PageModel
     {
+        [BindProperty]
+        public AddressModel Address { get; set; }
         public void OnGet()
         {
-
+        }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+            return RedirectToPage("/index");
         }
     }
 }
